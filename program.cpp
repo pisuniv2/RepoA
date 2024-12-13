6b596a2ff9076eb48d8d646ad1f80ef5e1daa517
Nikita Kholchev <nikitabel66@mail.ru>
2024-12-13 15:42:02 +0500

    Merged division branch into master

--- program.cpp
+++ program.cpp
@@ -1,11 +1,16 @@
-// Эта программа является учебным примером
-// Холчев Никита кэ-217
+// Программа является учебной
+// Холчев Никита КЭ-217
+// Год 2024
+
 #include <iostream>
 using namespace std;
+
 int main() {
     int a, b;
     cout << "Enter A and B: ";
     cin >> a >> b;
-    cout << "A / B = " << a / b << '\n';
+    cout << "A + B = " << a + b << '\n';
+    cout << "A - B = " << a - b << '\n';
+    cout << "A / B = " << a / b << '\n';  // Деление
     return 0;
 }
